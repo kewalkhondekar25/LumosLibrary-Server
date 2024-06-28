0818b417963b45dbbfe71edcbbb88421068a9af9
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-29 01:47:03 +0530

    feat: mongodb connection added

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/lumouslibrary`)
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MONGODB CONNECTED! HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MONGODB CONNECTION ERROR", error);
