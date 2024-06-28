c3df069e078d9bafbadc4c4145fc48d0118fab3b
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-29 01:52:58 +0530

    feat: mongodb connection added

--- src/db/db.js
+++ src/db/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MONGODB CONNECTED! HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MONGODB CONNECTION ERROR", error);
     process.exit(1);
   }
-}
+};
 
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config({path: "../.env"});
 
-connectDB();
+connectDB()
+.then(() =>{
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`SERVER IS RUNNING ON PORT: ${process.env.PORT}`);
+  })
+}).catch(() => {
+  console.log("MONGO DB CONNECTION FAILED");
+});
