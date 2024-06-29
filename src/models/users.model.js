cc4c1a3e047e116e10af2cf08707c2aa4cf3a3e6
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-29 20:22:09 +0530

    feat: users schema password methods added

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
 
 const usersSchema = new mongoose.Schema({
   name: {
@@ -21,5 +22,18 @@ const usersSchema = new mongoose.Schema({
     ref: "Order"
   }]
 }, {timestamps: true});
+
+//hash pwd before saving
+usersSchema.pre("save", async function(next){
+  if(!this.isModified("password")){
+    return next();
+  };
+  this.password = bcrypt.hash(this.password, 10);
+});
+
+//method to verify pwd
+usersSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password);
+};
 
 export const Users = mongoose.model("Users", usersSchema);
