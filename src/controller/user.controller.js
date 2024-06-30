353d1250fd16f45b7aca51b7c6fcd92f713007c7
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-30 14:38:00 +0530

    feat: user middleware added

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -74,7 +74,12 @@ const loginUser = asyncHandler(async (req, res) => {
   });
 });
 
+const logoutUser = asyncHandler(async (req, res) => {
+
+});
+
 export {
   registerUser,
-  loginUser
+  loginUser,
+  logoutUser
 }
---
+++ src/middleware/user.middleware.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model";
+import asyncHandler from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  //try-catch?
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+  if(!token){
+    return res.status(401).json({
+      message: "Unauthorized Request"
+    });
+  };
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  const user = await User.findById(decodedToken?._id).select("-password");
+  if(!user){
+    return res.status(401).json({
+      message: "Invalid Access Token"
+    });
+  };
+  //add user obj to req
+  req.user = user;
+  next();
+});
+
+export default verifyJWT;
